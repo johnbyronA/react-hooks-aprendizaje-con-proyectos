140c2bf454a80a30fa59bdcc08a3db5dbb37a6be
John Byron <113152853+johnbyronA@users.noreply.github.com>
2023-03-10 22:44:13 -0500

    Agregado custom hook para contador con valor desestructurado

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CounterApp from './01-useState/CounterApp';
+//import CounterApp from './01-useState/CounterApp';
 import './index.css';
 //import {HooksApp} from './HooksApp';
+import CounterWithCustomHook from './01-useState/CounterWithCustomHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </React.StrictMode>
 );
 
